a239d7dbef6aaecb36438a4edc5d7310083df1b0
janet-rs <rsjanet332@gmail.com>
2020-10-29 19:27:31 -0600

    Se subió

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var bodyParser = require ('body-parser');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var acercadeRouter = require('./routes/acercade');
+var loginRouter = require('./routes/login');
+
 
 var app = express();
 //Inicializar la base de datos
@@ -26,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/acercade', acercadeRouter);
+app.use('/login',loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
